0869583e8393203deda69e2d67d5168fd34223a1
Alexey Antipov <aantipov@gmail.com>
2021-02-26 00:36:37 +0300

    cmake scripts added

---
+++ cmake/crosstools.cmake
@@ -0,0 +1,33 @@
+cmake_minimum_required(VERSION 3.12)
+
+include(CMakeForceCompiler)
+set(TOOLCHAIN_PREFIX "arm-none-eabi-")
+set(TOOLCHAIN_POSTFIX ".exe")
+
+set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc${TOOLCHAIN_POSTFIX}")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++${TOOLCHAIN_POSTFIX}")
+#set(CMAKE_OBJD "arm-none-eabi-g++.exe")
+
+set(CMAKE_C_COMPILER_FORCED TRUE) # force compiler to avoid failing compiler tests
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) # It is important for test crosscompiling
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(OBJCOPY "${TOOLCHAIN_PREFIX}objcopy${TOOLCHAIN_POSTFIX}")
+set(SIZE ${TOOLCHAIN_PREFIX}size${TOOLCHAIN_POSTFIX})
+
+
+function(make_hex target)
+add_custom_target(hex
+            DEPENDS ${target}
+            COMMAND ${OBJCOPY} -O ihex ${target} ${target}.hex
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+endfunction()
+
+function(make_size target)
+    add_custom_target(size
+            DEPENDS ${target}
+            COMMAND ${SIZE} ${target}
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+endfunction()
---
+++ cmake/stm32f103.cmake
@@ -0,0 +1,8 @@
+set (CHIP "stm32f103")
+set (BOARD "MAVBOARD103")
+set (CORTEX cortex-m3)
+set (FPU 0)
+set(BLD_PLATFORM_FLAGS "-mcpu=cortex-m3 -mthumb -mfloat-abi=soft -mfpu=fpv4-sp-d16")
+
+
+
