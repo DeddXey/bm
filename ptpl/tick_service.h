a0f6cdf165aab025f92f2db5c63c9a024e43e567
Alexey Antipov <aantipov@gmail.com>
2022-02-13 15:05:38 +0300

    Delay Timer added

--- ptpl/tick_service.h
+++ ptpl/tick_service.h
@@ -4,7 +4,6 @@
 template<typename I>
 class TickService
 {
-
   uint32_t system_ticks = 0;
 
 public:
@@ -29,5 +28,43 @@ public:
     return system_ticks;
   }
 };
+
+template<typename Tick>
+class DelayTimer
+{
+
+  Tick    &tick_;
+  uint32_t start_ticks;
+  uint32_t delay;
+  bool     in_progress = false;
+
+public:
+  DelayTimer(Tick &tick) : tick_(tick) {}
+
+  void start(uint32_t ms) noexcept
+  {
+    start_ticks = tick_.get_ticks();
+    delay       = ms;
+    in_progress = true;
+  }
+
+  bool is_expired()
+  {
+    if (in_progress) {
+      if ((tick_.get_ticks() - start_ticks) >= delay) {
+        in_progress = false;
+        con.debug() << "DelayTimerExpired\n";
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+template<typename T>
+DelayTimer<T> make_delay_timer(T& tick_service)
+{
+  return DelayTimer<T>(tick_service);
+}
 
 #endif // TICK_SERVICE_H
