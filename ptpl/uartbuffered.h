c3eb2043ac5fb18a65edea126f4c6d7f8bba1b3c
Alexey Antipov <aantipov@gmail.com>
2022-02-13 15:06:36 +0300

    fifo atomic changes

--- ptpl/uartbuffered.h
+++ ptpl/uartbuffered.h
@@ -1,134 +1,140 @@
 #ifndef UARTBUFFERED_H
 #define UARTBUFFERED_H
 
-
+#include "fifo_atomic.h"
 #include "usart.h"
-#include "fifo.h"
+#include "sync.h"
 
-template <typename Port,
-          int sizeTx = 255,
-          int sizeRx = 255>
-class UartBuffered {
+template<typename Port, int sizeTx = 255, int sizeRx = 255>
+class UartBuffered
+{
 
-    tl::Fifo<uint8_t, sizeTx> txBuffer;
-    tl::Fifo<uint8_t, sizeRx> rxBuffer;
-    bool txIdle = true; ///< Флаг опустошения очереди.
-    bool rxIdle = false; ///< Флаг опустошения очереди.
-    bool ovr = false;
-
+  tl::fifo_atomic<uint8_t, sizeTx> txBuffer;
+  tl::fifo_atomic<uint8_t, sizeRx> rxBuffer;
+  bool txIdle = true;  ///< Флаг опустошения очереди.
+  bool rxIdle = false; ///< Флаг опустошения очереди.
+  bool ovr    = false;
 
 public:
-    using UPort = Port;
+  using UPort = Port;
 
+  void init()
+  {
+    Port::init();
+  }
 
+  //    tl::fifo_atomic<uint8_t, sizeRx>& getRxFifo()
+  auto getRxFifo() -> decltype(rxBuffer) &
+  {
+    return rxBuffer;
+  }
 
-    tl::Fifo<uint8_t, sizeRx>& getRxFifo()
-    {
-        return rxBuffer;
+  [[nodiscard]] bool overrun() const
+  {
+    return ovr;
+  }
+
+  void clearRxQueue()
+  {
+    rxBuffer.clear();
+    Port::setRxNotEmptyIt(true);
+  }
+
+  void clearTxQueue()
+  {
+    txBuffer.clear();
+  }
+
+  void clearOverrun()
+  {
+    ovr = false;
+  }
+
+  [[nodiscard]] bool empty() const
+  {
+    return rxBuffer.empty();
+  }
+
+  //    tl::fifo_atomic<uint8_t, sizeTx>& getTxFifo()
+  auto getTxFifo() -> decltype(txBuffer) &
+  {
+    return txBuffer;
+  }
+
+  bool &getTxIdle()
+  {
+    return txIdle;
+  }
+
+  bool &getRxIdle()
+  {
+    return txIdle;
+  }
+
+  void putChar(const uint8_t ch)
+  {
+    tl::critical_section cs;
+    if (txIdle) {
+      txIdle = false;
+      Port::putDr(ch);
+      Port::setTxEmptyIt(true);
+    }
+    else {
+      while (!txBuffer.push(ch))
+        ;
+      //            getTxFifo().push(ch);
+    }
+  }
+
+  bool getChar(uint8_t &out)
+  {
+    //      tl::critical_section cs;
+//    if (rxBuffer.empty())
+//      return false;
+    auto const out_o = rxBuffer.getOut();
+    if (out_o) {
+      Port::setRxNotEmptyIt(true);
+      out = out_o.value();
+      rxBuffer.pop();
+      return true;
+    }
+    return false;
+  }
+
+  int getTxFifoCapacity()
+  {
+    return txBuffer.size();
+  }
+
+  void irqHandler()
+  {
+    if (Port::isOverrun()) {
+      ovr = true;
     }
 
-    bool overrun() const
-    {
-        return ovr;
+    if (Port::isTxEmpty()) {
+
+      auto out = txBuffer.getOut();
+      if (out) {
+        Port::putDr(out.value());
+        txBuffer.pop();
+      }
+      else {
+        txIdle = true;
+        Port::setTxEmptyIt(false);
+      }
     }
 
-    void clearRxQueue()
-    {
-        rxBuffer.clear();
-        Port::setRxNotEmptyIt(true);
+    if (Port::isRxNotEmpty()) {
+      if (getRxFifo().full()) {
+        Port::setRxNotEmptyIt(false);
+      }
+      //            else {
+      uint8_t d = Port::getDr();
+      getRxFifo().push(d);
+      //            }
     }
-
-    void clearTxQueue()
-    {
-        txBuffer.clear();
-    }
-
-    void clearOverrun()
-    {
-        ovr = false;
-    }
-
-    bool empty() const
-    {
-        return getRxFifo().empty();
-    }
-
-    tl::Fifo<uint8_t, sizeTx>& getTxFifo()
-    {
-        return txBuffer;
-    }
-
-    bool& getTxIdle()
-    {
-        return txIdle;
-    }
-
-    bool& getRxIdle()
-    {
-        return txIdle;
-    }
-
-    void putChar(const uint8_t ch)
-    {
-      tl::critical_section cs;
-        if (getTxIdle()) {
-            getTxIdle() = false;
-            Port::putDr(ch);
-            Port::setTxEmptyIt(true);
-        }
-        else {
-            while(!getTxFifo().push(ch));
-        }
-    }
-
-    bool getChar(uint8_t & out)
-    {
-      tl::critical_section cs;
-        if (getRxFifo().empty())
-            return false;
-
-        Port::setRxNotEmptyIt(true);
-
-        out = getRxFifo().getOut();
-        getRxFifo().pop();
-        return true;
-
-    }
-
-    int getTxFifoCapacity()
-    {
-        return getTxFifo().size();
-    }
-
-    void irqHandler()
-    {
-        if (Port::isOverrun()) {
-            ovr = true;
-        }
-
-
-        if (Port::isTxEmpty()) {
-
-            if (getTxFifo().empty()) {
-                getTxIdle() = true;
-                Port::setTxEmptyIt(false);
-            }
-            else {
-                Port::putDr(getTxFifo().getOut());
-                getTxFifo().pop();
-            }
-        }
-
-        if (Port::isRxNotEmpty()) {
-            if (getRxFifo().full()) {
-                Port::setRxNotEmptyIt(false);
-            }
-            //            else {
-            volatile uint8_t d = Port::getDr();
-            getRxFifo().push(d);
-            //            }
-        }
-    }
+  }
 };
 
 #endif // UARTBUFFERED_H
