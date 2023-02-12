1008ad04fb307b933da38949c4316667e5dd86cf
Alexey Antipov <aantipov@gmail.com>
2023-02-12 13:06:06 +0300

    Noreturn added to the faults

--- mcu/stm32f429/include/it.h
+++ mcu/stm32f429/include/it.h
@@ -132,10 +132,10 @@ namespace Isr {
 
   void Reset();
   void Nmi();
-  void HardFault();
-  void MemManage();
-  void BusFault();
-  void UsageFault();
+  [[noreturn]] void HardFault();
+  [[noreturn]] void MemManage();
+  [[noreturn]] void BusFault();
+  [[noreturn]] void UsageFault();
   void Svc();
   void DebugMon();
   void PendSv();
